13d2b007b185a759e746c9fc2e86ff41348e69dc
Sabrina Rodriguez Sereno <92268628+sabrinarodriguez25@users.noreply.github.com>
2024-09-11 21:56:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+//Variables let
+let saludar="dice hola";
+let numero=4;
+
+let esEstudiante=true;
+
+let ciudad="Medellin";
+
+let calificacion= 4.5;
+
+let edad=25;
+
+let libros =["El principito", "Deja de ser tu", "el club de ña 5 am"]
+
+
+let altura=1.76;
+
+let profesion= "Militar";
+
+let año=2024;
+
+//Variables const
+
+const Nombre="sabrina Rodriguez"
+const cedula=10167868;
+const salario=100000;
+const edad1=20;
+const carrera="Desarrollo software"
+
+//Operaciones
+
+let presentacion =Nombre +"vive en"+ ciudad;
+console.log(presentacion)
+
+libros.push("3 años de soledad")
+console.log(libros)
+
+esEstudiante=false;
+console.log(esEstudiante)
+
+let union=Nombre + "tu calificación fue :" + calificacion;
+console.log(union)
+
+let suma =edad1+edad+ numero;
+
+
+//Operario ternario
+
+let Mayoredad = edad >= 18 ? "Es mayor de edad": "Es menor de edad";
+console.log(Mayoredad)
+let estadoEstudiante = esEstudiante ? "Es estudiante": "No es estdiante";
+console.log(estadoEstudiante)
